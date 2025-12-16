4c157adea9acdeb155f558d2c5a74be390ec5f27
abhishek.sankhla <abhisheksankhl121@gmail.com>
2025-12-16 18:35:09 +0530

    add delivery backend routes

--- controller/deliveryController.js
+++ controller/deliveryController.js
@@ -1,5 +1,60 @@
 import { catchAsyncError } from "../middleware/catchAsyncError.js";
+import Order from '../models/order.js'
+import ErrorHandler from "../utils/errorHandler.js";
 
-export const getDeliveryUser =catchAsyncError( async (req,res,next)=>{
-    return res.status(200).json("delivery found")
+export const getUnassignedOrder =catchAsyncError( async (req,res,next)=>{
+    const data = await Order.find({
+        isLocked: false
+    })
+    return res.status(200).json({
+    success: true,
+    message: "fecth delivery order successfully !",
+    data 
+})
+})
+
+
+
+export const assignOrder = catchAsyncError(async(req,res,next)=>{
+    const {id} = req.body
+    console.log("assign order: order_id:",id)
+    if(!id) return next(new ErrorHandler("Order id must require",400));
+    const data = await Order.findOneAndUpdate(
+        { _id: id, 
+          isLocked:false
+        },
+        {
+        isLocked: true,
+        deliveryPartner: req.user._id,
+        status: "ASSIGNED"
+        },
+        {
+            new:true
+        }
+     )
+  if (!data) {
+    return next(
+      new ErrorHandler("Order already assigned or not found", 400)
+    );
+  }
+ 
+    return res.status(200).json({
+    success: true,
+    message: "Assign delivery order successfully !",
+    data 
+})
+
+})
+
+export const updateOrderStatus= catchAsyncError(async(req,res,next)=>{
+    const {id,status} = req.body
+    console.log("assign order: order_id , status:",id,status)
+
+
+    
+    return res.status(200).json({
+    success: true,
+    message: "update delivery order status successfully !",
+    // data 
+})
 })
--- routes/deliveryRoutes.js
+++ routes/deliveryRoutes.js
@@ -1,8 +1,14 @@
 import express from 'express'
 
-import { getDeliveryUser } from '../controller/deliveryController.js';
+import { assignOrder, getUnassignedOrder, updateOrderStatus } from '../controller/deliveryController.js';
+import { isAuthenticated } from '../middleware/auth.js';
+import { checkRole } from '../middleware/checkRole.js';
 
 const router = express.Router();
-router.route('/').get(getDeliveryUser)
+router.route('/order')
+.get(isAuthenticated,checkRole(["DELIVERY"]),getUnassignedOrder )
+.post(isAuthenticated,checkRole(["DELIVERY"]),assignOrder)
+.patch(isAuthenticated,checkRole(["DELIVERY"]),updateOrderStatus)
+
 
 export default router  
